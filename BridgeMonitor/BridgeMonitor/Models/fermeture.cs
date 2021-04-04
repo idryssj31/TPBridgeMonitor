696578831e6fb4f1ed2fdbd8707c07fad94238ce
idryssj31 <idryss3108@gmail.com>
2021-04-04 18:07:49 +0200

    Qlq correction pour fermeture du pont et du fichier HomeController et ajout du Models

---
+++ BridgeMonitor/BridgeMonitor/Models/fermeture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BridgeMonitor.Models
+{
+    public class fermeture
+    {
+        public List<Bridge> BridgesBefore { get; set; }
+        public List<Bridge> BridgesAfter { get; set; }
+    }
+}
